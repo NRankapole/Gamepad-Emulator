bd2a37085f4399712acf54a9b52da288617c7275
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-02-19 14:03:17 +0200

    Troy | skeleton for Gameboard

---
+++ gamepad/src/components/Gameboard/Gameboard.tsx
@@ -0,0 +1,13 @@
+import type { Pos } from "../../Game"
+
+type Props = {
+  pos: Pos;
+  boardSize: number;
+  cellSize: number;
+};
+
+export function GameBoard({ pos, boardSize, cellSize }: Props) {
+  return (
+   
+  );
+}
