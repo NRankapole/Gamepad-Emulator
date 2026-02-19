ed674c3fd8b8124fda6363402b9caca81a0d61b4
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-02-19 14:03:42 +0200

    Troy | skeleton for Game

--- gamepad/src/Game.tsx
+++ gamepad/src/Game.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react";
+import { GameBoard } from "./components/Gameboard/Gameboard";
+import { Gamepad } from "./components/Gamepad/Gamepad";
 
-const Game = () => {
+export type Pos = {
+  x: number;
+  y: number;
+};
+
+const BOARD_SIZE = 8;
+const CELL_SIZE = 50;
+
+export default function Game() {
+  const [pos, setPos] = useState<Pos>({ x: 0, y: 0 });
+
+  function moveUp() {
+   
+  }
+
+  function moveDown() {
+  }
+
+  function moveLeft() {
+  }
+
+  function moveRight() {
+  }
+
   return (
-    <div>Game</div>
-  )
-}
+    <div>
+      <GameBoard pos={pos} boardSize={BOARD_SIZE} cellSize={CELL_SIZE} />
 
-export default Game
+      <Gamepad
+        onUp={moveUp}
+        onDown={moveDown}
+        onLeft={moveLeft}
+        onRight={moveRight}
+      />
+    </div>
+  );
+}
